e36588ced7b52079f97952a203f45271888ef0d7
ducks-monkeys123 <rhanerojan@gmail.com>
2025-05-01 13:45:45 +1200

    Initial commit.

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+export default function DashboardPage() {
+  return (
+  <Typography>
+    
+  </Typography>
+  );
+}
---
+++ src/pages/scan.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+export default function ScanPage() {
+  return <Typography>Scan QR code</Typography>;
+}
